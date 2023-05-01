053f1946ef5d5f88c43ddff4f686ab748c03b193
Anita Alves da Nóbrega Donato <anitaalves65097@gmail.com>
2023-05-01 01:20:27 -0300

    Atividades

---
+++ atividade3.py
@@ -0,0 +1,6 @@
+n1=int(input('Digite o valor de n1: '))
+if(n1>0):
+  print('Valor positivo')
+else:
+  print('Valor negativo')
+  
---
+++ atividade5.py
@@ -0,0 +1,6 @@
+salario=float(input('Informe seu salário: '))
+despesas=float(input('Informe suas despesas: '))
+if (salario>despesas):
+  print('Gastos dentro do orçamento')
+else:
+  print('Despesas maiores que o orçamento')
