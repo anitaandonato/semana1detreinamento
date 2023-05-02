50c16c801d3d362b1ac6d6333ec7b00afd838438
Anita Alves da Nóbrega Donato <anitaalves65097@gmail.com>
2023-05-02 15:57:16 -0300

    Corrigindo

--- atividade7.py
+++ atividade7.py
@@ -1,3 +1,3 @@
 frase=str(input('Diga uma frase: '))
 total_sem_espaços=len(frase)-frase.count(" ")
-print("Total sem espaços: ", total_sem_espaços)
+print("Total sem espaços:{} ".format(total_sem_espaços))
--- atividade9.py
+++ atividade9.py
@@ -3,6 +3,6 @@ contador = 0
 for char in string:
     if char.isalpha():
         contador+=1
-print('A quantidade de letras: '+contador)
+print('A quantidade de letras:{} '.format(contador))
 
 
