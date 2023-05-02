93bdd7b5f5e04def972133b564785fab7e699374
Anita Alves da Nóbrega Donato <anitaalves65097@gmail.com>
2023-05-01 23:35:14 -0300

    Atividades

---
+++ atividade6.py
@@ -0,0 +1,6 @@
+ano=int(input('Informe seu ano de nascimento: '))
+idade=2023-ano
+if(idade<18):
+    print('Menor de idade: {}'.format(idade))
+else:
+    print('Maior de idade: {}'.format(idade))
---
+++ atividade7.py
@@ -0,0 +1,3 @@
+frase=str(input('Diga uma frase: '))
+total_sem_espaços=len(frase)-frase.count(" ")
+print("Total sem espaços: ", total_sem_espaços)
